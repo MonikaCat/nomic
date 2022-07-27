5a012d83d587048e43400e7e3dc22ebf5cb7f4ff
Magic Cat <pusz.monika@gmail.com>
2022-07-27 12:15:19 +0100

    add commission and identity display to validator list

--- src/bin/nomic.rs
+++ src/bin/nomic.rs
@@ -484,8 +484,8 @@ impl ValidatorsCmd {
             let info: DeclareInfo =
                 serde_json::from_slice(validator.info.bytes.as_slice()).unwrap();
             println!(
-                "- {}\n\tVOTING POWER: {}\n\tMONIKER: {}\n\tDETAILS: {}",
-                validator.address, validator.amount_staked, info.moniker, info.details
+                "- {}\n\tVOTING POWER: {}\n\tMONIKER: {}\n\tCOMMISSION: {}\n\tIDENTITY: {}\n\tDETAILS: {}",
+                validator.address, validator.amount_staked, info.moniker, validator.commission.rate, info.identity, info.details
             );
         }
 
